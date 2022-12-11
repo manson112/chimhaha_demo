c55537517f74b721721519486f5153316e4f09a1
manson112 <rlarhksdn777@naver.com>
2022-12-11 14:47:13 +0900

    first commit

---
+++ src/main/java/manson112/github/chimhahademo/ChimhahademoApplication.java
@@ -0,0 +1,13 @@
+package manson112.github.chimhahademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimhahademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChimhahademoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/manson112/github/chimhahademo/ChimhahademoApplicationTests.java
@@ -0,0 +1,13 @@
+package manson112.github.chimhahademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimhahademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
